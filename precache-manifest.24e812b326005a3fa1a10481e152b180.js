ad4b8de8d07e6df245bb9695eeb4f30ce71cf690
Jason Novak <novak.jason7@gmail.com>
2019-10-01 22:00:04 -0400

    Updates

---
+++ precache-manifest.24e812b326005a3fa1a10481e152b180.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e53c7dff8c386b6815994a44930a6c",
+    "url": "/novakjason/clicky-game/index.html"
+  },
+  {
+    "revision": "c096affd0b3b722c16a9",
+    "url": "/novakjason/clicky-game/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "58ce3585960bf51b327f",
+    "url": "/novakjason/clicky-game/static/js/2.16fd8e67.chunk.js"
+  },
+  {
+    "revision": "c096affd0b3b722c16a9",
+    "url": "/novakjason/clicky-game/static/js/main.e85e1bff.chunk.js"
+  },
+  {
+    "revision": "39efd625bab436725615",
+    "url": "/novakjason/clicky-game/static/js/runtime-main.0057e6d1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/novakjason/clicky-game/static/media/logo.25bf045c.svg"
+  }
+]);
